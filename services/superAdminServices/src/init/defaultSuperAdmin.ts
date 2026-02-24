7d8d5381e94a65d8ff5aaff8e4d93b29b476c5bb
hritik7388 <choreohritik52@gmail.com>
2026-02-24 10:47:18 +0530

    fix default superadmin

--- services/superAdminServices/src/init/defaultSuperAdmin.ts
+++ services/superAdminServices/src/init/defaultSuperAdmin.ts
@@ -1,91 +1,65 @@
 import { AppDataSource } from '../data-source';
 import {
-    SuperAdmin,
-    UserType,
-    SuperAdminStatus,
+  SuperAdmin,
+  UserType,
+  SuperAdminStatus,
 } from '../entities/superAdmin.enities';
 import { SuperAdminCredential } from '../entities/superAdmin.credentials';
 import bcrypt from 'bcryptjs';
 import logger from '../config/logger';
 
 export const createDefaultSuperAdmin = async (): Promise<void> => {
-    const queryRunner = AppDataSource.createQueryRunner();
+  const superAdminRepo = AppDataSource.getRepository(SuperAdmin);
 
-    await queryRunner.connect();
-    await queryRunner.startTransaction();
+  // Efficient existence check
+  const existingAdmin = await superAdminRepo.findOne({
+    where: { userType: UserType.SUPER_ADMIN },
+  });
 
-    try {
-        const superAdminRepo = queryRunner.manager.getRepository(SuperAdmin);
-        const credentialRepo =
-            queryRunner.manager.getRepository(SuperAdminCredential);
+  if (existingAdmin) {
+    logger.info('Default SuperAdmin already exists. Skipping seeding.');
+    return;
+  }
 
-        const existingAdmin = await superAdminRepo.findOne({
-            where: { userType: UserType.SUPER_ADMIN },
-            relations: ['credential'],
-        });
+  const defaultEmail = process.env.DEFAULT_SUPERADMIN_EMAIL;
+  const defaultPassword = process.env.DEFAULT_SUPERADMIN_PASSWORD;
 
-        if (existingAdmin) {
-            logger.info('Default SuperAdmin already exists. Skipping seeding.');
-            await queryRunner.rollbackTransaction();
-            return;
-        }
+  if (!defaultEmail || !defaultPassword) {
+    throw new Error(
+      'DEFAULT_SUPERADMIN_EMAIL and DEFAULT_SUPERADMIN_PASSWORD must be defined.'
+    );
+  }
 
-        const defaultEmail = process.env.DEFAULT_SUPERADMIN_EMAIL;
-        const defaultPassword = process.env.DEFAULT_SUPERADMIN_PASSWORD;
+  const passwordHash = await bcrypt.hash(defaultPassword, 12);
 
-        if (!defaultEmail || !defaultPassword) {
-            throw new Error(
-                'DEFAULT_SUPERADMIN_EMAIL and DEFAULT_SUPERADMIN_PASSWORD must be defined in environment variables.'
-            );
-        }
+  const credential = new SuperAdminCredential();
+  credential.email = defaultEmail;
+  credential.passwordHash = passwordHash;
 
-        const passwordHash = await bcrypt.hash(defaultPassword, 12);
+  const superAdmin = new SuperAdmin();
+  superAdmin.firstName = 'Default';
+  superAdmin.lastName = 'SuperAdmin';
+  superAdmin.phoneNumber = '9999999999';
+  superAdmin.countryCode = '+91';
+  superAdmin.userType = UserType.SUPER_ADMIN;
+  superAdmin.status = SuperAdminStatus.ACTIVE;
+  superAdmin.isVerified = true;
+  superAdmin.address = {
+    street: 'Head Office',
+    city: 'City',
+    state: 'State',
+    postalCode: '000000',
+    country: 'Country',
+  };
+  superAdmin.coordinates = {
+    lat: 0,
+    lng: 0,
+  };
+  superAdmin.credential = credential;
 
-        const credential = credentialRepo.create({
-            email: defaultEmail,
-            passwordHash,
-        });
+  await superAdminRepo.save(superAdmin);
 
-        const superAdmin = superAdminRepo.create({
-            credential,
-            firstName: 'Default',
-            lastName: 'superAdmin',
-            userType: UserType.SUPER_ADMIN,
-            phoneNumber: '7388503329',
-            countryCode: '+91',
-            status: SuperAdminStatus.ACTIVE,
-            isVerified: true,
-            address: {
-                street: '123 Admin St',
-                city: 'Admin City',
-                state: 'Admin State',
-                postalCode: '12345',
-                country: 'Admin Country',
-
-            },
-            coordinates: {
-                lat: 40.7128,
-                lng: -74.0060,
-            },
-        });
-
-        await superAdminRepo.save(superAdmin);
-
-        await queryRunner.commitTransaction();
-
-        logger.info('Default SuperAdmin created successfully', {
-            email: defaultEmail,
-        });
-    } catch (error: any) {
-        await queryRunner.rollbackTransaction();
-
-        logger.error('Failed to seed Default SuperAdmin', {
-            message: error.message,
-            stack: error.stack,
-        });
-
-        throw error;
-    } finally {
-        await queryRunner.release();
-    }
+  logger.info('Default SuperAdmin created successfully', {
+    email: defaultEmail,
+  });
 };
