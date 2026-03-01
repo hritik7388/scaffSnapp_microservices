dedf9ccfae3ea2cff1acf0a43ce9557b6c17082d
choreohritik52@gmail.com <pushparaj@123>
2026-03-01 11:05:09 +0530

    done

--- services/subAdminService/src/types/express/index.d.ts
+++ services/subAdminService/src/types/express/index.d.ts
@@ -4,6 +4,7 @@ declare global {
     namespace Express {
         export interface Request {
             userId?: number; // optional to handle public routes where userId might not exist
+            userRole?: string;
             token?: string;  // optional for the same reason
         }
     }
