a37b89d49ff3d7d6f4a6b85a7c527a0c6cf09247
hritik7388 <choreohritik52@gmail.com>
2026-03-06 17:18:21 +0530

    fix issue

--- services/superAdminServices/src/schemas/superAdminSchema.ts
+++ services/superAdminServices/src/schemas/superAdminSchema.ts
@@ -52,7 +52,10 @@ export const resetPasswordSchema = z.object({
     .min(8, "Password must be at least 8 characters")
     .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
     .regex(/[a-z]/, "Password must contain at least one lowercase letter")
-    .regex(/[0-9]/, "Password must contain at least one number"),
+    .regex(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&]).{8,}$/,
+      "Password must contain uppercase, lowercase, number and special character"
+    )
 
 
 });
